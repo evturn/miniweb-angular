665d1a0e69fac36f59ceb5f24a0c2ccea8391fab
Evan Turner <evturn@gmail.com>
2015-02-26 20:47:15 -0500

    route to '/' sends users to 'pages/home.html where the mainController handles the

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var miniWeb = angular.module('miniWeb', []);
+var miniWeb = angular.module('miniWeb', ['negRoute']);
+
+miniWeb.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		});
+});
+
+
+
 
 miniWeb.controller('mainController', function($scope) {
 	$scope.message = "Dude, please stop looking at my site. Thanks.";
